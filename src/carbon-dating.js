5c26d6ae131626b1d53f24c1d62b06fff4d875ef
velichkoroman <tehvel@mail.ru>
2021-03-03 09:17:08 +0300

    feat: carbon-dating fixed final

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,7 +4,8 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  if ( !sampleActivity || typeof (sampleActivity)!=="string"){ return false};
+  
+  if ( !sampleActivity || typeof (sampleActivity)!=="string" || sampleActivity<=0 || sampleActivity>15 ||isNaN(Number(sampleActivity))){ return false};
 
   let k = 0.693/HALF_LIFE_PERIOD;
   
