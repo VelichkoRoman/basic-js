376542c2311b15c2ddbd346078fadcb05a4e20c5
velichkoroman <tehvel@mail.ru>
2021-03-03 11:41:17 +0300

    feat: dream-team some ver

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,8 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(members) {
+  let newArr=[];
+  if (typeof members!=='string' || members.length <= 0) {return false};
+  if (typeof members ==='string'){
   
-  if (members!=='string') {return false};
-
-
+  let preSorted = members.filter(
+     if (element==="string") { return element}
+     else {return false});
+   
+  newArr = preSorted.forEach((element) => element.slice(0,1).toUpperCase)).sort().join('');
+   
+  }; 
 };
+
