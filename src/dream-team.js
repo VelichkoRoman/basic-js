817fc5a310900616d77041d5483d4b4485e5f0ee
velichkoroman <tehvel@mail.ru>
2021-03-03 21:41:02 +0300

    feat: dream-team fix code

--- src/dream-team.js
+++ src/dream-team.js
@@ -4,17 +4,17 @@ const CustomError = require("../extensions/custom-error");
 // 'AABDEEGKPP' );
 module.exports = function createDreamTeam(members) {
    
- let newArr=[];
-if (!members) return false;
- for (let i = 0; i < members.length; i++) {
-  if (typeof (members[i]) == 'string') {
-      let preArr = members[i].slice(0,1).toUpperCase();
-      newArr.push(preArr);
-      
-  }
-  
- 
-} return newArr.sort();
+  let newArr=[];
+  if (!members)return false;
+  for (let i = 0; i < members.length; i++) {
+   if (typeof (members[i]) == 'string') {
+       let preArr = members[i].slice(0,1).toUpperCase();
+       newArr.push(preArr);
+       
+   }
    
+   console.log(newArr.sort().join(''));
+   
+ }return newArr.sort().join('');
 };
 
