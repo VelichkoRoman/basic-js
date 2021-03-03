1f7a4ab4a835940bfe7dff03f23c5073ab343fbb
velichkoroman <tehvel@mail.ru>
2021-03-03 09:21:31 +0300

    feat: count-cats solution

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,5 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  
+module.exports = function countCats(matrix) {
+  let count=0;
+  let cat=matrix.flat();
+
+  cat.forEach ((el) => {
+    if (el=="^^"){
+      count++;
+      return
+    }
+  })
+return count;
 };
