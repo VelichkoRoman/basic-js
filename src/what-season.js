a37c4271e51348826c194627d6aa8a75fe7dc74d
velichkoroman <tehvel@mail.ru>
2021-03-04 20:14:35 +0300

    feat: fix what-season

--- src/what-season.js
+++ src/what-season.js
@@ -2,8 +2,16 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
 
-
-  if (!date) return 'Unable to determine the time of year!' ;
-
   
+  if (date) {
+    if( !Date.prototype.getTime()) { throw new Error};
+    
+    let numMonth = date.getMonth() 
+        if (numMonth>=2 && numMonth<=4) {return 'spring';}
+        else if (numMonth>=5 && numMonth<=7) {return 'summer';}
+        else if (numMonth>=8 && numMonth<=10) {return 'autmn';}
+        else if(numMonth==11 || numMonth<=1) {return 'winter';}
+  }
+    else {return 'Unable to determine the time of year!'};
+
 };
