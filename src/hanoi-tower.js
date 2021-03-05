968938f9768963183b22a3ece00d40181ca57c90
velichkoroman <tehvel@mail.ru>
2021-03-05 14:28:17 +0300

    feat: fixed hanoi-tower

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -3,11 +3,11 @@ const CustomError = require("../extensions/custom-error");
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
   let turns;
   let seconds;
-if (disksNumber=0 || turnsSpeed!=0){
+if (disksNumber!==undefined){
 
  turns = Math.pow(2,disksNumber)-1;
  seconds = Math.floor((turns*3600)/turnsSpeed);
 
 }
-  return {turns, seconds};
+  return {turns, seconds };
 };
