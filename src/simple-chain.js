46dfcb80aa4aed4c81c5ce62360a1b6babc539da
VelichkoRoman <78850665+VelichkoRoman@users.noreply.github.com>
2021-03-08 19:57:01 +0300

    feat: add version simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,20 +1,40 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  newArr:[],
+  
   getLength() {
-    
+    return this.newArr.length;
   },
+
   addLink(value) {
-    
+    this.newArr.push(`( ${value} )`);
+    return this;   
   },
+
   removeLink(position) {
-    
+    if (!isNaN(position)) {
+      for (let i = 0; i < this.newArr.length; i++) {
+        if (i == position - 1) {
+          this.newArr.splice(i, 1);
+        }
+      }
+    } else {
+      this.newArr = [];
+      throw new Error('Error');
+    }
+    return this;
   },
+
   reverseChain() {
-   
+    this.newArr.reverse();
+    return this;
   },
+
   finishChain() {
-   
+    let end = this.newArr.join("~~");
+    this.chain = [];
+    return end;
   }
 };
 
