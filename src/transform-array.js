2592477e2173470f4461942961b7370d440b4649
VelichkoRoman <78850665+VelichkoRoman@users.noreply.github.com>
2021-03-08 19:05:19 +0300

    feat: add transform-array commities is brake

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+
+  let newArr = arr.slice(0);
+
+  for (let i = 0; i < newArr.length; i++) {
+    if (newArr[i] === '--discard-next' ) {
+      if (i == newArr.length - 1 ) {newArr[i]='none'}
+      else {
+        newArr[i]='none';
+        newArr[i+1]='none';
+      }
+    }
+
+    else if (newArr[i] === '--discard-prev' ) {
+      if (i == 0 ) { newArr[i]='none'}
+      else {
+        newArr[i] ='none';
+        newArr[i-1] ='none';
+      }
+    }   
+
+    else if (newArr[i] === '--double-next') {
+      if (i == newArr.length -1 ) { newArr[i] = 'none'}
+      else { newArr[i]=newArr[i+1]}
+    }
+
+    else if (newArr[i] === '--double-prev') {
+      if (i == 0) { newArr[i]= 'none'}
+      else{ newArr[i]=newArr[i-1]}
+    }
+  }
+  
+    return newArr.filter(value => value != 'none');  
+  
 };
