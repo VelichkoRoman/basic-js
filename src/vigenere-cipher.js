6d512180326b162f460838f765f2d7e552064e88
VelichkoRoman <78850665+VelichkoRoman@users.noreply.github.com>
2021-03-08 19:18:52 +0300

    feat: begin vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+   encrypt(enc) {
+    if (enc==null) throw new Error ('Error');
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(dec) {
+    if (dec==null) throw new Error ('Error');
   }
 }
 
